2a73ea21a6009bf710b0a8edf9d5aa16d7140d6c
Larios.Psd <106607142+Mlarios-2021097@users.noreply.github.com>
2023-10-17 03:27:05 -0600

    config.js

--- config.js
+++ config.js
@@ -7,47 +7,49 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
+global.botnumber = ""
+
 global.owner = [
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', false],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417', '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐', true],
-  ['50253501417'],
-  ['50253501417'],
-  ['50253501417'],
-  ['50253501417'],
-  ['50253501417'],
-  ['50253501417'],
-  ['50253501417'],
-  ['50253501417'], 
-  ['50253501417'],  
-  ['50253501417'], 
-  ['50253501417']
+  ['5219992095479', '👑 Mystic - Creador 👑', true],
+  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
+  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
+  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
+  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
+  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
+  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
+  ['5215533827255', '💫 Mystic - Collaborator 7 💫', false],
+  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
+  ['393518398856','💫 Mystic - Tester Fabri115💫', true],
+  ['593968585283'],
+  ['5219993404349'],
+  ['5219991402134'],
+  ['5492266466080'],
+  ['5219996125657'],
+  ['5218442114446'],
+  ['59894808483'],
+  ['593980586516'], 
+  ['595975740803'],  
+  ['5492266613038'], 
+  ['595992611272']
 ];
 
-global.suittag = ['50253501417'];
-global.prems = ['50253501417'];
+global.suittag = ['5219993404349'];
+global.prems = ['51995386439'];
 
-global.packname = '@𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃';
-global.author = '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐';
-global.wm = '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐';
-global.titulowm = '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐';
-global.titulowm2 = `⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐`
-global.igfg = '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐';
+global.packname = '(☞ﾟ∀ﾟ)☞';
+global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★';
+global.wm = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★';
+global.titulowm = '🤖 ＴＨＥ ＭＹＳＴＩＣ － ＢＯＴ 🤖';
+global.titulowm2 = `乂  𝚃 𝙷 𝙴  𝙼 𝚈 𝚂 𝚃 𝙸 𝙲  -  𝙱 𝙾 𝚃  乂`
+global.igfg = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★';
 global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
 
-global.imagen1 = fs.readFileSync('./Larios.PNG');
-global.imagen2 = fs.readFileSync('./src/Larios.PNG');
-global.imagen3 = fs.readFileSync('./src/Larios.PNG');
-global.imagen4 = fs.readFileSync('./Larios.PNG');
-global.imagen5 = fs.readFileSync('./src/Larios.PNG');
-global.imagen6 = fs.readFileSync('./Larios.PNG');
+global.imagen1 = fs.readFileSync('./Menu2.jpg');
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg');
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png');
+global.imagen4 = fs.readFileSync('./Menu.png');
+global.imagen5 = fs.readFileSync('./src/+18.jpg');
+global.imagen6 = fs.readFileSync('./Menu3.png');
 
 global.mods = [];
 
@@ -60,9 +62,9 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `▸ ${dia} ${fecha}\n▸ ⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐;
-global.gt = '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐';
-global.mysticbot = '⭐ @𝐋𝐀𝐑𝐈𝐎𝐒.𝐏𝐒𝐃 ⭐';
+global.wm2 = `▸ ${dia} ${fecha}\n▸ 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝`;
+global.gt = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★';
+global.mysticbot = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★';
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.waitt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*';
