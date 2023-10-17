c3f474021e9a56ef67c50132feecf9d9f08f297a
Larios.Psd <106607142+Mlarios-2021097@users.noreply.github.com>
2023-10-17 02:16:51 -0600

    index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const {name, author} = require(join(__dirname, './package.json'));
 const {say} = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
-say('Larios.Psd - Bot\nWhatsApp Bot ', {
+say('LariosPsd - Bot\nWhatsApp Bot MD', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
